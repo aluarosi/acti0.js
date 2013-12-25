d31998a55c38b2b112bd2b5c1e494835663bf70c
Alvaro Santamaria Herrero <aluarosi@gmail.com>
2013-12-25 23:41:16 +0100

    d3 micro test

--- src/js/setup_d3test.js
+++ src/js/setup_d3test.js
@@ -25,10 +25,70 @@
     Shows interactive circles for each viewpoint
  */
 
-define(['d3'], function(d3){
+define(['d3'], function(d3dummy){
+    // d3.js seems not to be imported the require.js way
+    // d3 is in the global scope instead
+
+    var my_viewpoints = {};
 
     var setup_d3test = function(thisapp){
         console.log("d3test"); 
+
+        //TODO: this should be cloned
+        my_viewpoints = thisapp.config.viewpoints; 
+        // A shared model should exist to coordinate this display
+        //  with other controls tigeering the camera movements
+        //  but we "model" the state hacking the config object!!!
+        //  This is pretty ugly. We want something running fast.
+        var div_viewpoints = d3.selectAll("#div_viewpoints");
+        var w = div_viewpoints[0][0].clientWidth;
+        var h = div_viewpoints[0][0].clientHeight;
+        var n = my_viewpoints.length;
+        var inc = h/n;
+        var r = inc*0.8;
+        var set_cx = function(d,i){
+            var cx = (i/3|0)*w/2 + w/4; 
+            return cx;
+        };
+        var set_cy = function(d,i){
+            var j = i/3|0;
+            var k = i -j*3;
+            var cy = 2*inc*(k+1/2); 
+            return cy;
+        };
+    
+        // Trying out D3.js for the 1st time
+        // Round buttons to select the camra viewpoint
+        var circles = div_viewpoints.append("svg")
+            .selectAll("circle")
+            .data(my_viewpoints)
+            .enter().append("circle")
+            .attr("cx", set_cx)
+            .attr("cy", set_cy)
+            .attr("r", r)
+            .style("fill", "orange")
+            .style("stroke", "grey")
+            .on("click", function(d,i){
+                console.log(i);
+                thisapp.shared.goToViewpoint(thisapp.config.viewpoints[i]);
+                // Ugly hack, as we tamper config
+                my_viewpoints.forEach(function(d,i){
+                    d.__selected__ = false;
+                });
+                my_viewpoints[i].__selected__ = true;
+                update();
+            });
+            
+        var update = function(i){
+            var circles = div_viewpoints.select("svg").selectAll("circle")
+                .data(my_viewpoints)
+                .transition().duration(1000)
+                .style("fill", function(d,i){
+                    return d.__selected__ ? "green" : "orange";
+                });
+            
+        };
+        
 
     };
     return setup_d3test;
