e4dd6f3ea46e9dd51eb27f18c5a1b04c551e2807
Alvaro Santamaria Herrero <aluarosi@gmail.com>
2014-01-06 02:36:20 +0100

    1st build

---
+++ build/build.js
@@ -0,0 +1,14 @@
+({
+    baseUrl: "./",
+    paths: {
+        jquery  : "lib/jquery",
+        three   : "lib/three_r63",
+        orbitpan    : "lib/OrbitAndPanControls.new",
+        datgui  : "lib/dat.gui.min",
+        tweenjs : "lib/tweenjs",
+        kinetic : "lib/kinetic",
+        d3      : "lib/d3"
+    },
+    name: "main",
+    out: "main-built.js"
+})
